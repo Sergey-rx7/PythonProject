bb9a2a1799aed863ee2bb24df285bff8446482e6
sk-pc <sergey-rx7@yandex.ru>
2019-10-20 17:01:44 +0300

    Git Files

---
+++ ml/textConvertor.py
@@ -0,0 +1,10 @@
+class textToList:
+    # entryText = str("Some text for function")
+    entryText = input(str())
+
+    def textSplit(self):
+        return self.entryText.split()
+
+
+textList = textToList()
+print(textList.textSplit())
