817ba6f1791b41ba21ad50e4b54e9dbbecf88295
sergey.klishch <sergey-rx7@yandex.ru>
2020-01-24 16:44:37 +0300

    changed structure and add counter

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 set1 = set(['1', '2', '3'])
-set2 = (['1', '3', '4'])
+set2 = (['1', '4', '3'])
 
 
 # sel = (['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
@@ -7,9 +7,12 @@ set2 = (['1', '3', '4'])
 
 
 def my_function(set2):
-    if len(set1.intersection(set2)) > 0:
+    if len(set1.intersection(set2)) > 2:
         # return "YES"
         print("YES")
+    else:
+        print(len(set1.intersection(set2)))
+
 
 
 my_function(set2)
